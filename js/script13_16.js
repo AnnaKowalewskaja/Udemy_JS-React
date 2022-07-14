086439f223e57340aaee9c073284a622792b3e10
Kowalewskajaaa <kowalewskajaanna@gmail.com>
2022-07-14 21:58:35 +0200

    continue 16

---
+++ js/script13_16.js
@@ -0,0 +1,58 @@
+"use strict";
+//13
+
+// alert("hello");
+
+//const res= confirm("Are you here?");    //question yes-no
+//console.log(res);//true or false
+
+// const answer=prompt("How are you?","good");//good-default
+// console.log(answer); //always typeOf-string
+
+
+//const answers=[];
+// answers[0] =prompt("what your name?","");
+// answers[1] =prompt("how are you?","");
+// answers[2] =prompt("are you here?","");
+//document.write(answers);
+
+// console.log(typeof(answers));//object
+// console.log(typeof(null));//object
+
+//14 интерполяция
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user=prompt("What your name?","USER");
+// alert(`Hello, ${user}`);
+
+//15 операторы 
+// 
+
+// incr++; //11
+// decr--;//9
+
+// console.log(incr,decr); //11,9
+//console.log(++incr,--decr)//11,9
+//console.log(incr++,decr--)//10,10
+//console.log(5%2);//1
+
+//= рауенство
+// ==сравнение, без приведения типов данных
+// === строгое сравнение, с приведением типов даннных
+
+// && and запинается на лжи и возвращает первое ложное значение
+// || or  запинает на правде и возвращает первое правдивое значение
+
+//16
+
+//git init
+//git config  --local user.name "ivan"
+//git config  --local user.email ivan@gmail.com
+//если установлен локальный,то глобальный не будет записан
+
+//git status
+//git add -A   //все файлы добавляются в индекс, гит-статус
+
+//git commit -a -m"init:first commit"
