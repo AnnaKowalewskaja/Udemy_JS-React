228b45a57872c3e6a90688344a05ac1bc94122e1
Kowalewskajaaa <kowalewskajaanna@gmail.com>
2022-07-14 21:08:48 +0200

    continue:2. 1-12

---
+++ js/script1_12.js
@@ -0,0 +1,66 @@
+"use strict";
+//console.log("hello");
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+// console.log(obj);
+
+// console.log(name);
+// var name = 'one';
+
+//////типы данных
+//число
+// let number = 4.6;
+// console.log(-4/0); //infiniti
+//console.log('string'*7);//NaN
+
+//строка
+//const persone='Alex';
+
+//bool
+//const bool=true;
+
+//undefined
+//коробка пуста, но существует
+// let und;
+// console.log(und);
+
+//something is not defined
+//коробка пуста и не существует
+//console.log(something);
+
+//Object
+//действия-методы
+//данные-свойства
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false,
+// };
+
+// console.log(obj.name);
+
+//массив-это частный случай объекта,со свойствами 0,1,2,3...
+
+// let arr = ['first', 'second', 0, {}, [], 'third'];
+// console.log(arr[1]);
+
+//let storeName="goodLood";
+// const storeDescription={
+//     budget:10000,
+//     employees:["John","Ula","Dima"],
+//     products:{
+//         orange:30,
+//         cucumber:20,
+//     },
+//     open:true,
+// };
