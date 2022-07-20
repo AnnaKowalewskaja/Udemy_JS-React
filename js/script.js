1a2858d63e2f2cf8027375d74ebd4a897ba203b7
Anna <kowalewskajaanna@gmail.com>
2022-07-20 22:33:09 +0200

    feat:lesson 24,25

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+////////////////24  Практика, ч2. Применяем условия и циклы
+
 /* Задание на урок:
 
 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
@@ -15,3 +17,97 @@
 'use strict';
 
 // Код возьмите из предыдущего домашнего задания
+
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {
+
+//     },
+//     actors: {
+
+//     },
+//     genres: [],
+//     privat: false,
+// }
+
+// // 1 способ
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt("Один из последних просмотренных фильмов?", ""),
+//      b = prompt("На сколько оцените его?", "");
+//     if (a === null || a === "" || a.length > 50 || b === null || b === "" || b.length > 50) {
+//         i--;
+//         continue;
+//     }
+//     let scoreLastFilms = prompt("На сколько оцените его?", "");
+//     personalMovieDB.movies[lastFilms] = +scoreLastFilms;
+
+// }
+
+// //2 способ
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt("Один из последних просмотренных фильмов?", ""),
+//      b = prompt("На сколько оцените его?", "");
+//      if(a != null && a!= '' && a.length <= 50 && b != null && b!= '' && b.length <= 50){
+//         personalMovieDB.movies[a] = +b;
+//         console.log('done')
+//      }else{
+//          console.log('error');
+//          i--;
+//      }
+// }
+
+// if (personalMovieDB.count < 10) {
+//     alert('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+//     alert('Вы классический зритель');
+// }else if(personalMovieDB.count >= 30){
+//     alert('Вы киноман');
+// }else{
+//     alert('Произошла ошибка');
+// }
+
+//     console.log(personalMovieDB);
+
+
+
+// /////////////////////////25. Функции, стрелочные ф-ции (ES6)
+// let num = 20;
+// function showFirstMessage(text){
+//     console.log(text);
+//     let num = 30;
+//     console.log(num);
+// }
+// h
+// showFirstMessage("hello,world!");
+// console.log(num);
+
+// function calc(a,b){
+//     return a+b;
+// }
+// console.log(calc(3,7));
+// console.log(calc(13,7));
+// console.log(calc(3,57));
+
+//function declaretion
+// //можно вернуть какие-то данные после их работы в функции
+// function ret(){
+//     let num = 50;
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+//function expression
+// const logger = function(){
+// console.log("logger")
+// };
+
+// logger();
+
+//arrow function
+//dont have  'this'
+//const calc = (a, b) => a + b;
