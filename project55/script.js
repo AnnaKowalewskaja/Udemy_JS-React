f455f90f06dfd73e279cf623f5d8c5b6cae8712c
Anna <kowalewskajaanna@gmail.com>
2022-11-24 22:38:02 +0100

    fix:55

---
+++ project55/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const boxesQuery = document.querySelectorAll('.box');
+const boxesGet = document.getElementsByClassName('box'); //коллекция следит за элементами, а массив-нет
+
+boxesQuery.forEach(box => {
+    if (box.matches('.this')) {
+        console.log('this');
+    }
+});
+//matches - содержит ли такой атрибут
+
+console.log(boxesQuery[0].closest('.wrapper')); //ближайший с классом
+
+// boxesQuery[0].remove();
+// boxesGet[0].remove();
+
+// for(let i = 0;i<5;i++){
+//     const div = document.createElement('div');
+//     div.classList.add('box');
+//    // document.body.append(div);
+//   // boxesGet[boxesGet.length] =div;. //так нельзя потому что коллекция
+// }
+
+// console.log(boxesQuery);
+// console.log(boxesGet);
+//console.log(document.body.children);
+
+//console.log(Array.from(boxesGet));//чтобы получить массив из коллекции
