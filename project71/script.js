97fe840d6b3017d102de64a06d48a575e5ce1e71
Anna <kowalewskajaanna@gmail.com>
2023-05-09 20:37:51 +0200

    fix:71

---
+++ project71/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const box = document.querySelector('.box'),
+    btn = document.querySelector('button');
+const width = box.clientWidth;
+const height = box.scrollHeight;
+
+
+btn.addEventListener('click', () => {
+    box.style.height = box.scrollHeight+'px';
+    //console.log(box.scrollTop);
+});
+
+//console.log(box.getBoundingClientRect().top);
+
+// const style = window.getComputedStyle(box); //from css, no inline
+// console.log(style.display);
+
