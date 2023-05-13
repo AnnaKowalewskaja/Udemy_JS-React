3572a1d030f010b7b615a973b02427df8ff25a27
Anna <kowalewskajaanna@gmail.com>
2023-05-13 16:51:57 +0200

    fix:77-80,test

--- project74/script.js
+++ project74/script.js
@@ -97,33 +97,147 @@
 
 
 
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
 
-// btn.addEventListener('click',function(){
-//     this.style.backgroundColor='red';
-// });
+// // btn.addEventListener('click',function(){
+// //     this.style.backgroundColor='red';
+// // });
 
 
-// btn.addEventListener('click',() =>{
-//     this.style.backgroundColor='red';
-// });
-//TypeError: undefined is not an object (evaluating 'this.style.backgroundColor='red'')
+// // btn.addEventListener('click',() =>{
+// //     this.style.backgroundColor='red';
+// // });
+// //TypeError: undefined is not an object (evaluating 'this.style.backgroundColor='red'')
 
-//у стрелочный ункции нет контекста
+// //у стрелочный ункции нет контекста
 
-const obj ={
-    num:5,
-    sayNumber:function(){
-        const say = () =>{
-            console.log(this);
-        };
-        say();
+// const obj ={
+//     num:5,
+//     sayNumber:function(){
+//         const say = () =>{
+//             console.log(this);
+//         };
+//         say();
+//     }
+// };
+
+// obj.sayNumber(); //{num: 5, sayNumber: function}
+// //стрелочная функция say взяла контекст у родителя.А так родитель-метод,то контекст-объект
+
+// const double = a => a*2;
+
+// console.log(double(4));
+
+////////////////////////////////////////////////////////////////////////////////////77
+// class Rectangle {
+//     constructor(height, width) {
+//         this.height = height;
+//         this.width = width;
+//     }
+//     calcArea() {
+//         return this.height * this.width;
+//     }
+// }
+
+
+// class ColoredRectangleWithText extends Rectangle {
+//     constructor(height, width, text, bgColor) {
+//         super(height,width);
+//         this.text = text;
+//         this.bgColor = bgColor;
+
+
+//     }
+// showMyProps(){
+//     console.log(`Text:${this.text}, color: ${this.bgColor}`);
+// }
+// }
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// // console.log(long.calcArea());
+// // console.log(square.calcArea());
+
+// const div = new ColoredRectangleWithText(25,10,"Hello",'red');
+// div.showMyProps();
+// console.log(div.calcArea());
+
+////////////////////////////////////////////////////////////////////////////////////80
+// const log = function(a){
+//     console.log(a);
+// };
+
+// //log(1,2,'o','e',1,3,4,5);  //12[oe1345]
+
+// function calcOrDouble(number,basis =2){
+//     log(number*basis);
+// }
+
+// calcOrDouble(3);
+
+
+///////////////////////////////////////////Test
+
+// function getSum(a, b) {
+//     function sum() {
+//         console.log(this.a);
+//         return a + b;
+//     }
+ 
+//     console.log(sum());
+// }
+ 
+// getSum(4, 5);
+
+// class Slider {
+//     constructor(width, height) {
+//         this.width = width;
+//         this.height = height;
+//     }
+ 
+//     showSliderWidth() {
+//         console.log(this.width);
+//     }
+// }
+ 
+// const slider = new Slider('500px', '150px');
+ 
+// slider.showSliderWidth();
+
+// const urlObj = {
+//     protocol: 'https',
+//     domain: 'mysite.com',
+//     showCurrentURL: function() {
+//         const extractCurrDomain = () => {
+//             return this.domain;
+//         }
+//         const extractCurrProtocol = () => {
+//             return this.protocol;
+//         }
+ 
+//         console.log(`${extractCurrProtocol()}://${extractCurrDomain()}`)
+//     }
+// }
+ 
+// urlObj.showCurrentURL();
+
+const urlObj = {
+    protocol: 'https',
+    domain: 'mysite.com'
+}
+ 
+function showCurrentURL() {
+    const extractCurrDomain = () => {
+        return this.domain;
     }
-};
-
-obj.sayNumber(); //{num: 5, sayNumber: function}
-//стрелочная функция say взяла контекст у родителя.А так родитель-метод,то контекст-объект
-
-const double = a => a*2;
-
-console.log(double(4));
+    const extractCurrProtocol = () => {
+        return this.protocol;
+    }
+ 
+    console.log(`${extractCurrProtocol()}://${extractCurrDomain()}`)
+}
+ 
+const url = showCurrentURL.bind(urlObj);
+ 
+console.log(url);
