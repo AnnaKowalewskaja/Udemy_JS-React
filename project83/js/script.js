8ba9034d5e1e0d134d032056bba94d5726b913f1
Anna <kowalewskajaanna@gmail.com>
2023-05-22 17:44:13 +0200

    fix:95

--- project83/js/script.js
+++ project83/js/script.js
@@ -1,28 +1,29 @@
 "use strict";
 
-const inputRub = document.querySelector('#rub'),
-    inputUsd = document.querySelector('#usd');
+// const inputRub = document.querySelector('#rub'),
+//     inputUsd = document.querySelector('#usd');
 
-inputRub.addEventListener('input', () => {
-    const request = new XMLHttpRequest();
-    request.open('GET', 'js/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset = utf-8');
-    request.send();
-
-
-request.addEventListener('readystatechange',()=>{
-if(request.readyState===4 && request.status ===200){
-    console.log(request.response);
-    const data =JSON.parse(request.response);
-    inputUsd.value =(+inputRub.value/data.current.usd).toFixed(2);
-}else{
-    inputUsd.value='Error';
-}
-});
-    //status
-    //statusText
-    //response    (answer)
-    //readyState
+// inputRub.addEventListener('input', () => {
+//     const request = new XMLHttpRequest();
+//     request.open('GET', 'js/current.json');
+//     request.setRequestHeader('Content-type', 'application/json; charset = utf-8');
+//     request.send();
 
 
-});
+// request.addEventListener('readystatechange',()=>{
+// if(request.readyState===4 && request.status ===200){
+//     console.log(request.response);
+//     const data =JSON.parse(request.response);
+//     inputUsd.value =(+inputRub.value/data.current.usd).toFixed(2);
+// }else{
+//     inputUsd.value='Error';
+// }
+// });
+//     //status
+//     //statusText
+//     //response    (answer)
+//     //readyState
+
+
+// });
+
