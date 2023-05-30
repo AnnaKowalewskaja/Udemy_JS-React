46942f3d5ada220193f067dbeb47ec3ea8794b61
Anna <kowalewskajaanna@gmail.com>
2023-05-30 19:46:06 +0200

    fix:107

---
+++ project107/script.js
@@ -0,0 +1,61 @@
+// 'use strict';
+
+// const data = [
+//     {
+//         id: 'box',
+//         tag: 'div'
+//     },
+//     {
+//         id: '',
+//         tag: 'nav'
+//     },
+//     {
+//         id: 'circle',
+//         tag: 'span'
+//     }
+// ];
+
+// data.forEach((blockObj,i) => {
+//     const block = document.createElement(blockObj.tag);
+//     //if(!blockObj.id) throw new Error(`In object number ${i+1} no id`);
+//     block.setAttribute('id', blockObj.id);
+//     document.body.append(block);
+// });
+
+// const err = new SyntaxError('kkkkk');
+// console.log(err.name,err.message,err.stack);
+
+
+'use strict';
+
+const data = [{
+        id: 'box',
+        tag: 'div'
+    },
+    {
+        id: '',
+        tag: 'nav'
+    },
+    {
+        id: 'circle',
+        tag: 'span'
+    }
+];
+
+
+
+// const err = new SyntaxError('kkkkk');
+// console.log(err.name,err.message,err.stack);
+
+try {
+    data.forEach((blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+        if (!blockObj.id) throw new SyntaxError(`In object number ${i+1} no id`);
+        block.setAttribute('id', blockObj.id);
+        document.body.append(block);
+    });
+} catch (e) {
+    if(e.name ==="SyntaxError"){
+        console.log(e.message);
+    }else throw e;
+}
