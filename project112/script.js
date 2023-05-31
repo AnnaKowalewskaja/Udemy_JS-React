622947dd14da23dbbf8708348f9cb6ef7cd4c865
Anna <kowalewskajaanna@gmail.com>
2023-05-31 22:51:39 +0200

    fix:113

---
+++ project112/script.js
@@ -0,0 +1,41 @@
+ 'use strict';
+
+// const btnPhone = document.querySelector('#iphone'),
+//     btnMacbook = document.querySelector('#macbook'),
+//     images = document.querySelectorAll('img');
+
+
+
+
+
+// let phoneAnimation;
+// btnPhone.addEventListener('click', () => {
+//     if (!phoneAnimation) {
+//         phoneAnimation = images[0].animate([{
+//                 transform: 'translateY(0) rotate(0deg)',
+//                 filter: 'opacity(100%) '
+//             },
+//             {
+//                 transform: 'translateY(100px) rotate(180deg)',
+//                 filter: 'opacity(70%) '
+//             },
+//             {
+//                 transform: 'translateY(-100px) rotate(360deg)',
+//                 filter: 'opacity(40%) '
+//             },
+//             {
+//                 transform: 'translateY(0) rotate(180deg)',
+//                 filter: 'opacity(70%) '
+//             },
+//         ], {
+//             duration: 3000,
+//             iterations: Infinity,
+//         });
+//     } else if (phoneAnimation.playState === 'paused') {
+//         phoneAnimation.play();
+//     } else {
+//         phoneAnimation.pause();
+//     }
+// });
+
+
--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 
 Project 'Food'
-
 https://annakowalewskaja.github.io/Udemy_JS-React/project64/
+
+
+Animation Buttons
+https://annakowalewskaja.github.io/Udemy_JS-React/project112/
