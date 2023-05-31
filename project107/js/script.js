6de1e6027527cf56eec8ecf34b26cbbb9c5ec83c
Anna <kowalewskajaanna@gmail.com>
2023-05-31 22:07:38 +0200

    fix:110

--- project107/js/script.js
+++ project107/js/script.js
@@ -73,8 +73,32 @@ Vue.JS
  2) Webpack
  3) NodeJS
 
-
-
-
-
 */
+
+////////////////////////////////////////////////////////////110
+//npm i jquery --save
+
+
+import $ from 'jquery';
+
+// const btn = $('#btn');
+// console.log(btn);
+
+$(document).ready(function () {
+    $('.list-item:first').hover(function () {
+        $(this).toggleClass('active');
+    });
+
+    $('.list-item:eq(2)').on('click', function () {
+        $('.image:even').fadeToggle();
+  
+    });
+
+    $('.list-item:eq(4)').on('click', function () {
+        $('.image:odd').animate({
+            opacity:'toggle',
+            height:'toggle'
+        },2000);
+   
+     });
+});
