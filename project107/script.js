4d1fcd9afb583794fbe2403b11db40e202882f3d
Anna <kowalewskajaanna@gmail.com>
2023-05-31 20:32:51 +0200

    fix:109

--- project107/script.js
+++ project107/script.js
@@ -1,7 +1,34 @@
+// // 'use strict';
+
+// // const data = [
+// //     {
+// //         id: 'box',
+// //         tag: 'div'
+// //     },
+// //     {
+// //         id: '',
+// //         tag: 'nav'
+// //     },
+// //     {
+// //         id: 'circle',
+// //         tag: 'span'
+// //     }
+// // ];
+
+// // data.forEach((blockObj,i) => {
+// //     const block = document.createElement(blockObj.tag);
+// //     //if(!blockObj.id) throw new Error(`In object number ${i+1} no id`);
+// //     block.setAttribute('id', blockObj.id);
+// //     document.body.append(block);
+// // });
+
+// // const err = new SyntaxError('kkkkk');
+// // console.log(err.name,err.message,err.stack);
+
+
 // 'use strict';
 
-// const data = [
-//     {
+// const data = [{
 //         id: 'box',
 //         tag: 'div'
 //     },
@@ -15,47 +42,39 @@
 //     }
 // ];
 
-// data.forEach((blockObj,i) => {
-//     const block = document.createElement(blockObj.tag);
-//     //if(!blockObj.id) throw new Error(`In object number ${i+1} no id`);
-//     block.setAttribute('id', blockObj.id);
-//     document.body.append(block);
-// });
-
-// const err = new SyntaxError('kkkkk');
-// console.log(err.name,err.message,err.stack);
 
 
-'use strict';
+// // const err = new SyntaxError('kkkkk');
+// // console.log(err.name,err.message,err.stack);
 
-const data = [{
-        id: 'box',
-        tag: 'div'
-    },
-    {
-        id: '',
-        tag: 'nav'
-    },
-    {
-        id: 'circle',
-        tag: 'span'
-    }
-];
+// try {
+//     data.forEach((blockObj, i) => {
+//         const block = document.createElement(blockObj.tag);
+//         if (!blockObj.id) throw new SyntaxError(`In object number ${i+1} no id`);
+//         block.setAttribute('id', blockObj.id);
+//         document.body.append(block);
+//     });
+// } catch (e) {
+//     if(e.name ==="SyntaxError"){
+//         console.log(e.message);
+//     }else throw e;
+// }
+
+////////////////////////////////////////////////////////////109
+/*   React
+1) JSX
+2) Babel
+3) Webpack
+4) React
+5) NodeJS
+
+Vue.JS
+ 1) JS
+ 2) Webpack
+ 3) NodeJS
 
 
 
-// const err = new SyntaxError('kkkkk');
-// console.log(err.name,err.message,err.stack);
 
-try {
-    data.forEach((blockObj, i) => {
-        const block = document.createElement(blockObj.tag);
-        if (!blockObj.id) throw new SyntaxError(`In object number ${i+1} no id`);
-        block.setAttribute('id', blockObj.id);
-        document.body.append(block);
-    });
-} catch (e) {
-    if(e.name ==="SyntaxError"){
-        console.log(e.message);
-    }else throw e;
-}
+
+*/
