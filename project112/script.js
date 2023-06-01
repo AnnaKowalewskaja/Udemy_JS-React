5e2dd6640dd7e655e1a39c7ddf88f11de9ccec79
Anna <kowalewskajaanna@gmail.com>
2023-06-01 11:12:16 +0200

    fix:114

--- project112/script.js
+++ project112/script.js
@@ -1,5 +1,6 @@
  'use strict';
 
+
 // const btnPhone = document.querySelector('#iphone'),
 //     btnMacbook = document.querySelector('#macbook'),
 //     images = document.querySelectorAll('img');
@@ -38,4 +39,75 @@
 //     }
 // });
 
+
+///////////////////////////////
+// const multiply20 = (price) => price * 20;
+// const divide100 = (price) => price / 100;
+// const normalizePrice = (price) => price.toFixed(2);
+
+// const compose = (...fns) => (x) => fns.reduceRight((res, fn) => fn(res), x);
+
+// const composeWithArgs = (...fns) => fns.reduceRight((f, g) => (...args) => g(f(...args)));
+
+// const discount = compose(normalizePrice, divide100, multiply20);
+
+// discount(2000);
+
+
+///////////////////////////
+// const promisify = (item, delay) =>
+//     new Promise(resolve => setTimeout(() => resolve(item), delay));
+ 
+// const a = () => promisify('a', 100);
+// const b = () => promisify('b', 5000);
+// const c = () => promisify('c', 3000);
+ 
+// async function one() {
+//     const promises = [a(), b(), c()];
+//     const [outpu1, outpu2, outpu3] = await Promise.all(promises);
+//     return `one is done: ${outpu1} ${outpu2} ${outpu3}`;
+// }
+ 
+// async function two() {
+//     const promises = [a(), b(), c()];
+//     const outpu1 = await Promise.race(promises);
+//     return `two is done: ${outpu1}`;
+// }
+ 
+// async function three() {
+//     const outpu1 = await a();
+//     const outpu2 = await b();
+//     const outpu3 = await c();
+//     return `three is done: ${outpu1} ${outpu2} ${outpu3}`
+// }
+ 
+// one().then(console.log);
+// two().then(console.log);
+// three().then(console.log);
+//////////////////////////////////////////////////////114
+// console.log(1);
+
+// setTimeout(()=>{
+//     console.log('timeout');
+// },2000);
+
+// setTimeout(()=>{
+//     console.log('timeout__4000');
+// },4000);
+
+// console.log(2);
+
+// let k =0;
+// function count(){
+//     for(let i= 0; i<1e9 ;i++){
+//         k++;
+//     }
+//     alert('done');
+// }
+
+
+setTimeout(()=>{
+    console.log(1);
+},0);
+console.log(2);
 
