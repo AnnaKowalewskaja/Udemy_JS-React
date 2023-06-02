b34e111359fa52eca75de6c9d12bc2e328e2b938
Anna <kowalewskajaanna@gmail.com>
2023-06-02 22:47:33 +0200

    fix:123

--- project121/project121/src/App.js
+++ project121/project121/src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+const Header = () => {
+  return <h2>Hello</h2>
+}
+
+
+// const Field = () => {
+//   const holder = 'Enter here';
+//   const styledField = {
+//     width: '300px',
+//   }
+//   return <input placeholder={holder}
+//     type='text'
+//     style={styledField} />
+// }
+
+class Field extends Component{
+  render(){
+    const holder = 'Enter here';
+    const styledField = {
+      width: '300px',
+    };
+    return <input placeholder={holder}
+    type='text'
+    style={styledField} />
+  }
+  }
+
+function Btn() {
+  const text = 'Log in';
+
+  // const res =()=>{
+  //   return 'log in';
+  // }
+  //return <button>{res()}</button>
+  const p = <p>Log in</p>
+  const logged = false;
+
+  return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learns React
-        </a>
-      </header>
+      <Header />
+      <Field />
+      <Btn />
     </div>
   );
 }
-
+export {Header};
 export default App;
--- project121/project121/src/index.js
+++ project121/project121/src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App, { Header } from './App';
 import reportWebVitals from './reportWebVitals';
 
 
 //const elem = <h2>Hello World</h2>;
-const text = 'hello world';
-const elem =(
-  <div>
-    <h2>Text+{text}</h2>
-    <input type='text'/>
-    <button className='button'/>
-    <h2>Hello</h2>
+// const text = 'hello world';
+// const elem =(
+//   <div>
+//     <h2>Text+{text}</h2>
+//     <input type='text'/>
+//     <button className='button'/>
+//     <h2>Hello</h2>
     
-  </div>
-)
+//   </div>
+// )
 
 // const elem = React.createElement('h2', {
 //   classname: 'greetngs'
@@ -23,7 +23,8 @@ const elem =(
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <App/>
+  
 );
 
 
