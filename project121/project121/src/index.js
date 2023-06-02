6dfbf190b7ba2a2c3535eaa118363f7b95ff7fcc
Anna <kowalewskajaanna@gmail.com>
2023-06-02 22:16:59 +0200

    fix:122

--- project121/project121/src/index.js
+++ project121/project121/src/index.js
@@ -4,14 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+//const elem = <h2>Hello World</h2>;
+const text = 'hello world';
+const elem =(
+  <div>
+    <h2>Text+{text}</h2>
+    <input type='text'/>
+    <button className='button'/>
+    <h2>Hello</h2>
+    
+  </div>
+)
+
+// const elem = React.createElement('h2', {
+//   classname: 'greetngs'
+// }, 'hello world');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
