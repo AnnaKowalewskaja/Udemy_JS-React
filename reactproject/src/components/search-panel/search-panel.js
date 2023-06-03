2b7703d33dfaa24793f7154f1358ee768c0303f7
Anna <kowalewskajaanna@gmail.com>
2023-06-03 18:30:43 +0200

    fix:126

---
+++ reactproject/src/components/search-panel/search-panel.js
@@ -0,0 +1,17 @@
+
+import './search-panel.css';
+
+
+
+
+const SearchPanel = () =>{
+    return (
+        <input
+        type='text'
+        className="form-control search-input"
+        placeholder="Search employee"/>
+    )
+}
+
+
+export default SearchPanel;
