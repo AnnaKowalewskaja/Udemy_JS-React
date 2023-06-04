437917322049284be2848c459f25c9f080aed5e0
Anna <kowalewskajaanna@gmail.com>
2023-06-04 21:14:15 +0200

    fix:130

--- project121/project121/src/App.js
+++ project121/project121/src/App.js
@@ -1,21 +1,39 @@
 
+import { Component } from 'react';
 
+ 
+class WhoAmI extends Component{
+   constructor(props){
+    super(props);
+    this.state = {
+        years:27,
+       
+    }
+   }
 
-function WhoAmI({name,surname,link}){
+   nextYear = () =>{
+    console.log('+++');
+    this.setState({
+        years:this.state.years+1,
+    })
+   }
+   render(){
+    const {name,surname,link} = this.props;
     return (
         <div className="">
-            <h1>My name is {name()}, surname - {surname}</h1>
+            <button onClick={this.nextYear}>+++</button>
+            <h1>My name is {name}, surname - {surname},age-{this.state.years}</h1>
             <a href={link}>My profile</a>
         </div>
-    )
+    )}
 }
 
 
 function App(){
     return (
         <div className="App">
-            <WhoAmI name= {() => {return 'John'}} surname='Smith' link ='facebook.com'/>
-            <WhoAmI name= {() => {return 'Alex'}} surname='Shepard' link ='twitter.com'/>
+            <WhoAmI name= 'John' surname='Smith' link ='facebook.com'/>
+            <WhoAmI name= 'Alex' surname='Shepard' link ='twitter.com'/>
            
         </div>
     )
