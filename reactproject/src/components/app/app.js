26f9c084a1279d855cbea45569eb2ae10112c072
Anna <kowalewskajaanna@gmail.com>
2023-06-05 13:17:58 +0200

    fix:135

--- reactproject/src/components/app/app.js
+++ reactproject/src/components/app/app.js
@@ -1,34 +1,64 @@
 
+import { Component } from 'react';
 
 import AppFilter from '../app-filter/app-filter';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
-import './app.css';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
+import './app.css';
 
-function App() {
-    const data = [
-        { name: 'Ivan C.', salary: 750, increase:true, id:1},
-        { name: 'John C.', salary: 800, increase:false, id:2},
-        { name: 'Alex M.', salary: 700, increase:false, id:3},
-        { name: 'Anna C.', salary: 900, increase:true, id:4},
-      
-    ];
 
-    return (
-        <div className="app">
-            <AppInfo />
-            <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
 
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                { name: 'Ivan C.', salary: 750, increase: true, id: 1 },
+                { name: 'John C.', salary: 800, increase: false, id: 2 },
+                { name: 'Alex M.', salary: 700, increase: false, id: 3 },
+                { name: 'Anna C.', salary: 900, increase: true, id: 4 },
+
+            ]
+        }
+    }
+
+    deleteItem = (id) => {
+        //1     
+        //const index = data.findIndex(elem => elem.id === id);      
+        // const before = data.slice(0,index);
+        // const after = data.slice(index+1);
+        // const newArr =[...before,...after];
+        // return{
+        //     data:newArr
+        // }
+
+        //2
+        this.setState(({ data }) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+
+    render() {
+        return (
+            <div className="app">
+                <AppInfo />
+                <div className="search-panel">
+                    <SearchPanel />
+                    <AppFilter />
+
+                </div>
+                <EmployeesList data={this.state.data}
+                    onDelete={this.deleteItem} />
+                <EmployeesAddForm />
             </div>
-            <EmployeesList data={data} />
-            <EmployeesAddForm />
-        </div>
-    )
+        )
+    }
 }
 
 export default App;
--- reactproject/src/components/employees-list/employees-list.js
+++ reactproject/src/components/employees-list/employees-list.js
@@ -4,13 +4,17 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 import './employees-list.css';
 
 
-const EmployeesList = ({ data }) => {
+const EmployeesList = ({ data,onDelete }) => {
 
     const elements = data.map(item => {
         const {id,...itemProps} = item;
         return (
-            <EmployeesListItem key={id} {...itemProps} />
+            <EmployeesListItem 
+            key={id} 
+            {...itemProps}
+            onDelete={() => onDelete(id)} />
             //name={item.name} salary={item.salary}   === {...item}
+
         )
     })
     return (
