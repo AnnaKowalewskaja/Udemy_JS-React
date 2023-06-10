65aa427a3fe16e570f7fba8d55458e0181561750
Anna <kowalewskajaanna@gmail.com>
2023-06-10 18:51:49 +0200

    fix:140

--- reactproject/src/components/employees-list-item/employees-list-item.js
+++ reactproject/src/components/employees-list-item/employees-list-item.js
@@ -4,8 +4,8 @@
 import './employees-list-item.css';
 
 
-const EmployeesListItem =(props)=> {
-    
+const EmployeesListItem = (props) => {
+
 
     // onIncrease = () => {
     //     this.setState(({ increase }) => ({
@@ -20,42 +20,45 @@ const EmployeesListItem =(props)=> {
     // }
 
 
-    
 
-        const { name, salary, onDelete,onToggleProp, increase,rise  } = props;
-      
-        let classNames = 'list-group-item d-flex justify-content-between';
-    
-        if (increase) {
-            classNames += ' increase';
-        };
 
-        if (rise) {
-            classNames += ' like';
-        };
+    const { name, salary, onDelete, onToggleProp, increase, rise } = props;
 
-        return (
-            <li className={classNames}>
-                <span className='list-group-item-label' onClick={onToggleProp} data-toggle = 'rise' >{name}</span>
-                <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
-                <div className='d-flex justify-content-center align-items-center'>
-                    <button type="button"
-                        className="btn-cookie btn-sm "
-                        onClick={onToggleProp}
-                        data-toggle = 'increase'>
-                        <i className="fas fa-cookie"></i>
-                    </button>
+    let classNames = 'list-group-item d-flex justify-content-between';
 
-                    <button type="button"
-                        className="btn-trash btn-sm "
-                        onClick={onDelete}>
-                        <i className="fas fa-trash"></i>
-                    </button>
-                    <i className="fas fa-star"></i>
-                </div>
-            </li>
-        )
-    
+    if (increase) {
+        classNames += ' increase';
+    };
+
+    if (rise) {
+        classNames += ' like';
+    };
+
+    return (
+        <li className={classNames}>
+            <span className='list-group-item-label'
+             onClick={onToggleProp} 
+             data-toggle='rise'
+             style = {{fontSize:23}} >{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
+            <div className='d-flex justify-content-center align-items-center'>
+                <button type="button"
+                    className="btn-cookie btn-sm "
+                    onClick={onToggleProp}
+                    data-toggle='increase'>
+                    <i className="fas fa-cookie"></i>
+                </button>
+
+                <button type="button"
+                    className="btn-trash btn-sm "
+                    onClick={onDelete}>
+                    <i className="fas fa-trash"></i>
+                </button>
+                <i className="fas fa-star"></i>
+            </div>
+        </li>
+    )
+
 }
 
 export default EmployeesListItem;
