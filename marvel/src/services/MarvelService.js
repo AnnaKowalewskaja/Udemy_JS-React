1d602577c481c300dc3988da7cafb864971dd233
Anna <kowalewskajaanna@gmail.com>
2023-07-08 21:51:58 +0200

    fix:154

--- marvel/src/services/MarvelService.js
+++ marvel/src/services/MarvelService.js
@@ -4,6 +4,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=a94a45cb582f455e1d79a4e176ddfc4e';
+_baseOffset = 210;
 
     getResourse = async (url) => {
         const res = await fetch(url);
@@ -14,8 +15,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
